70fc1cad2d2a8f2a0b7366180e78c73de282aac3
nemostatus <mattcorralejo1@gmail.com>
2021-03-25 02:24:34 -0700

    changed arrow syntax in order to make the code act as a constructor instead of a regular function testing methods out

--- queue.js
+++ queue.js
@@ -1,6 +1,27 @@
-let queue = () => {
+function queue(){
     let collection = []
     this.print = () => {
         console.log(collection)
     }
+    this.enqueue = element => {
+        collection.push(element) //add to end of collection, if i want to add to beginning i would use unshift
+    }
+    this.dequeue = () => {
+       return collection.shift() //removes element from beginning unlike stack which remove from end so
+        //queue - fifo first in first out
+        //stack - lifo last in first out
+    }
+    this.front = () => {
+       return collection[0]
+    }
+    this.size = () => {
+        return collection.length
+    }
+    this.isEmpty = () => {
+        return collection.length === 0 ? console.log("IS EMPTY"): console.log("IS NOT EMPTY")
+    }
 }
+
+let q = new queue
+q.isEmpty()
+
